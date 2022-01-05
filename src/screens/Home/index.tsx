1bc5a757bf2ee962d576aa3181cb71ea7505a9b8
Renan Loureiro <renanpl47@gmail.com>
2022-01-05 10:00:58 -0300

    created Header Component to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
+import { RFValue } from "react-native-responsive-fontsize"
 import Logo from "../../assets/logo.svg"
 
-import { Container, Header, Title } from "./styles"
+import { Container, Header, TotalCars, ContentHeader } from "./styles"
 
 export const Home = () => {
   return (
     <Container>
       <StatusBar style="light" backgroundColor="transparent" translucent />
       <Header>
-        <Logo />
-        <Title>Total de carros 12</Title>
+        <ContentHeader>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>Total de carros 12</TotalCars>
+        </ContentHeader>
       </Header>
     </Container>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,8 @@
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize"
 import styled from "styled-components/native"
 
+import { getStatusBarHeight } from "react-native-iphone-x-helper"
+
 export const Container = styled.View`
   flex: 1;
 
@@ -9,15 +11,20 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   width: 100%;
-  flex-direction: row;
   height: ${RFValue(113)}px;
   background: ${({ theme }) => theme.colors.header};
-  align-items: center;
-  justify-content: space-between;
-  padding: 69px 16px 32px 16px;
+  justify-content: flex-end;
 `
 
-export const Title = styled.Text`
+export const ContentHeader = styled.View`
+  width: 100%;
+  padding: 32px 24px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const TotalCars = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_400};
   color: ${({ theme }) => theme.colors.text};
   font-size: ${RFValue(15)}px;

4edd6b681675f7f49af82a5ea16ac3a6563346b7
Renan Loureiro <renanpl47@gmail.com>
2022-01-04 23:55:11 -0300

    created Header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,17 @@
+import { StatusBar } from "expo-status-bar"
 import React from "react"
+import Logo from "../../assets/logo.svg"
 
-import { Container, Title } from "./styles"
+import { Container, Header, Title } from "./styles"
 
 export const Home = () => {
   return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Header>
+        <Logo />
+        <Title>Total de carros 12</Title>
+      </Header>
     </Container>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,24 @@
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize"
 import styled from "styled-components/native"
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
+
   align-items: center;
+`
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
+  height: ${RFValue(113)}px;
+  background: ${({ theme }) => theme.colors.header};
+  align-items: center;
+  justify-content: space-between;
+  padding: 69px 16px 32px 16px;
 `
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_400};
-  color: ${({ theme }) => theme.colors.main};
-  font-size: 42px;
+  color: ${({ theme }) => theme.colors.text};
+  font-size: ${RFValue(15)}px;
 `

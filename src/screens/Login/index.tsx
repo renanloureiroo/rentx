ebecef18fec6998b5ee420cf02fd197ea98b75fd
Renan Loureiro <renanpl47@gmail.com>
2022-01-17 18:26:39 -0300

    validation form

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -6,6 +6,8 @@ import {
   Keyboard,
 } from "react-native"
 import { useTheme } from "styled-components"
+import * as Yup from "yup"
+
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
 import { PasswordInput } from "../../components/PasswordInput"
@@ -17,6 +19,26 @@ export const Login = () => {
   const [password, setPassword] = useState("")
 
   const theme = useTheme()
+
+  const handleSignIn = async () => {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required("E-mail é obrigatório")
+          .email("Digite uma e-mail válido"),
+
+        password: Yup.string().required("Senha é obrigatório"),
+      })
+
+      await schema.validate({ email, password })
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        // TODO
+      } else {
+        // TODO
+      }
+    }
+  }
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
